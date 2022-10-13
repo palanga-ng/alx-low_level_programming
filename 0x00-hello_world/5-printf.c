093d534764e32eb928550739a37b5e6e873aada2
jamilu shuaibu <palanga374@gmail.com>
2022-10-14 00:10:38 +0100

    my changes

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+/**
+* main - Prints a gramma text
+*
+* Return: Always 0 (Done)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
