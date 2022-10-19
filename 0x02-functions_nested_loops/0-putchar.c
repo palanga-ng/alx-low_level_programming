0b337734230b51c819dee27b8cf93503e734730a
jamilu shuaibu <palanga374@gmail.com>
2022-10-19 22:32:43 +0100

    my changes

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ *main - Entry point
+ * Return: Always 0 (success)
  */
 int main(void)
 {
