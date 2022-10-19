375244618297bd3e045100d54c7a6e190cff8a54
jamilu shuaibu <palanga374@gmail.com>
2022-10-19 23:11:26 +0100

    my changes

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,18 +9,20 @@
 int main(void)
 {
 	int i;
-	long int fibonacci[50], sum = 2;
+	long int fibonacci[50];
 
 	fibonacci[0] = 1;
 	fibonacci[1] = 2;
+	printf("%ld, %ld, ", fibonacci[0], fibonacci[1]);
 
 	for (i = 2; i < 50; i++)
 	{
 		fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
-		if ((fibonacci[i] % 2) == 0 && fibonacci[i] < 4000000)
-			sum += fibonacci[i];
+		if (i == 49)
+			printf("%ld\n", fibonacci[i]);
+		else
+			printf("%ld, ", fibonacci[i]);
 	}
-	printf("%ld\n", sum);
 
 	return (0);
 }
